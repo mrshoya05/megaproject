74db1b64814e7695116e3e730a117e719350d9fe
shoyab <shoyabkhan0508@gmail.com>
2024-01-31 10:01:17 -0800

    database setup in appwrite

---
+++ src/Appwrite/auth/authservice.js
@@ -0,0 +1,56 @@
+import conf from "../../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    Account;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+
+    }
+    async createAccount({email, password, name}){
+        try{
+       const userAccount =  await this.account.create(ID.unique(), email, password, name);
+       if (userAccount) {
+       // call another method 
+       return this.login({email, password});
+       
+       }else{
+        return userAccount;
+       }
+        }catch(error){
+           throw  error;
+        }
+    }
+    async login({email, password}){
+        try {
+          return  await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+
+    }
+    async getCurrentUser(){
+        try {
+            await this.this.account.get();
+        } catch (error) {
+            console.log("appwrite servive :: getCurrentUser :: error", error);
+        }
+        return null;
+    }
+    async logOut(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
+}
+const authService = new AuthService();
+
+export default authService;
+
